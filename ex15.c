bc430bc4e908fa28cfad368e58cb56cfa956bb57
roseviet <uyen.nguyencong@gmail.com>
2016-10-22 22:16:26 +0700

    Pointers Dreaded Pointers

---
+++ ex15.c
@@ -0,0 +1,72 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex15.c
+ *
+ *    Description:  Pointers Dreaded Pointers
+ *
+ *        Version:  1.0
+ *        Created:  10/22/2016 01:48:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  cue, 
+ *   Organization:  MIT
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // create two arrays we care about
+    int ages[] = {23, 43, 12, 89, 2};
+    char *names[] = {
+        "Alan", "Frank",
+        "Mary", "John", "Lisa"
+    };
+
+    // safely get the size of ages
+    int count = sizeof(ages) / sizeof(int);
+    int i = 0;
+
+    // first way using indexing
+    for(i = 0; i < count; i++) {
+        printf("%s has %d years alive.\n",
+                names[i], ages[i]);
+    }
+
+    printf("---\n");
+
+    // setup the pointers to the start of the arrays
+    int *cur_age = ages;
+    char **cur_name = names;
+
+    // second way using pointers
+    for(i = 0; i < count; i++) {
+        printf("%s is %d years old.\n",
+                *(cur_name + 1), *(cur_age+i));
+    }
+
+    printf("---\n");
+
+    // thrid way, pointers are just array
+    for (i = 0; i < count; i++) {
+        printf("%s is %d years old again.\n",
+                cur_name[i], cur_age[i]);
+    }
+
+    printf("---\n");
+
+    // fourth way with pointers in a stupid complex way
+    for(cur_name = names, cur_age =ages;
+            (cur_age - ages) < count;
+            cur_name++, cur_age++)
+    {
+        printf("%s lived %d years so far.\n",
+                *cur_name, *cur_age);
+    }
+
+    return 0;
+}
+
