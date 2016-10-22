ac8593eeaaf06ab268a1522f7ee44a6c7555a095
roseviet <uyen.nguyencong@gmail.com>
2016-10-22 12:12:53 +0700

    If, Else-If, Else

---
+++ ex12.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex12.c
+ *
+ *    Description:  If, Else-If, Else
+ *
+ *        Version:  1.0
+ *        Created:  10/22/2016 12:07:36 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  cue, 
+ *   Organization:  MIT
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    if(argc == 1) {
+        printf("You only have one argument. You suck.\n");
+    } else if(argc > 1 && argc < 4) {
+        printf("Here's your arguments:\n");
+
+        for(i = 0; i < argc; i++) {
+            printf("%s ", argv[i]);
+        }
+        printf("\n");
+    } else {
+        printf("You have too many arguments. You suck.\n");
+    }
+
+    return 0;
+}
