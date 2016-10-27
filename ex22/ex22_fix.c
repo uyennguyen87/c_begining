d8da8e198012a0bac6b045f6c8166c0847e54690
roseviet <uyen.nguyencong@gmail.com>
2016-10-27 22:45:55 +0700

    Exercise 22. Stack, Scope and Global

---
+++ ex22/ex22_fix.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "ex22.h"
+#include "dbg.h"
+
+int THE_SIZE = 1000;
+
+static int THE_AGE = 37;
+static double RATIO = 1.0;
+
+int get_age()
+{
+    return THE_AGE;
+}
+
+void set_age(int age)
+{
+    THE_AGE = age;
+}
+
+double get_ratio()
+{
+    return RATIO;
+}
+
+void set_ratio(double new_ratio)
+{
+    RATIO = new_ratio;
+}
+
+double update_ratio(double new_ratio)
+{
+    double old_ratio = get_ratio();
+    set_ratio(new_ratio);
+    return old_ratio;
+}
+
+void print_size()
+{
+    log_info("I think size is: %d", THE_SIZE);
+}
